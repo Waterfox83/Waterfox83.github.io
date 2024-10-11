e76e19471ca5ae565fca77e4495a273cce1f8042
Asthana, Abhishek <iabhishek@gmail.com>
2024-10-11 18:33:28 +0530

    removed an image

--- js/family-images.js
+++ js/family-images.js
@@ -4,7 +4,6 @@ const imagePaths = [
     'https://photosblob.blob.core.windows.net/photos/family/photo3.jpg',
     'https://photosblob.blob.core.windows.net/photos/family/photo4.jpg',
     'https://photosblob.blob.core.windows.net/photos/family/photo5.jpg',
-    'https://photosblob.blob.core.windows.net/photos/family/photo6.jpg',
-    'https://photosblob.blob.core.windows.net/photos/family/photo7.jpg'
+    'https://photosblob.blob.core.windows.net/photos/family/photo6.jpg'
     // Add more family image paths as needed
 ];
